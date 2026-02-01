572b9f06900048073448f654bc2e9816e1c5bcd3
Igor Pache <devigorpache@gmail.com>
2026-01-31 21:09:19 -0300

    SellerDaoJDBC and DaoFactory

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,4 +1,6 @@
 package application;
+import modelDao.DaoFactory;
+import modelDao.SellerDao;
 import modelEntities.Department;
 import modelEntities.Seller;
 import java.util.Date;
@@ -10,6 +12,8 @@ public class Main {
         Department obj = new Department(1, "Books");
 
         Seller seller = new Seller (21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        SellerDao sellerDao = DaoFactory.createSellerDao();
         System.out.println(seller);
     }
 }
